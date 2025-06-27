82e7e4a204bc643cda31b3fd1ff894e4befb0980
root <root@daphpve.com>
2025-06-27 14:32:22 +0200

    updated to link to FE

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import router from './router';
 const app = createApp(App);
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
+axios.defaults.baseURL = 'https://homegymbe.onrender.com';  // the FastAPI backend
 
 app.use(router);
 app.mount("#app");
