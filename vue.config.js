6e0201eaa3dc468a6c51db11edf7ec0b478f0104
RaphBourgeois <raphal.bourgeois@gmail.com>
2025-06-28 01:26:51 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,7 @@ const { defineConfig } = require('@vue/cli-service')
 }) */
     
     module.exports ={
-  server: {
-    proxy: {
-      // Proxy all requests starting with `/api` to your Laravel backend
-      '/api': {
-        target: 'https://homegymbe.onrender.com:5000', // Your Laravel backend URL
-        changeOrigin: true, // Needed for virtual hosted sites
-        secure: true, // If you're using HTTPS, set this to true
-        // Remove the rewrite function to preserve the `/api` prefix
-      },
-    },
-    
-  }
+        devServer: {
+          proxy: 'https://homegymbe.onrender.com/',
+       }
 };
