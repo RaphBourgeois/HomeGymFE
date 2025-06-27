6978ee6c7c07648858061567f46bde2dace9ad8c
root <root@daphpve.com>
2025-06-27 14:02:48 +0200

    ready to publish

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+/* module.exports = defineConfig({
+    transpileDependencies: true
+}) */
+
+    module.exports ={
+        transpileDependencies: true,
+        devServer: {
+            allowedHosts: [
+                '100.100.213.115',
+                '.ts.net',
+                '.myapp.com'
+            ]
+          }
+
+    }
