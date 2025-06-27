0932dda1c0045e88497ba860b11da0739e101fe7
RaphBourgeois <raphal.bourgeois@gmail.com>
2025-06-27 21:54:31 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 /* module.exports = defineConfig({
     transpileDependencies: true
 }) */
-
+    
     module.exports ={
         transpileDependencies: true,
         devServer: {
             allowedHosts: [
                 '100.100.213.115',
                 '.ts.net',
-                '.myapp.com'
+                '.myapp.com',
+                'https://homegymfe.netlify.app/'
             ]
           }
 
